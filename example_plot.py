ebcae61e4382f0017b9ad3abcc23f70764d90dd2
Ankit Kumar <akvyas1995@gmail.com>
2022-04-11 09:18:04 +0530

    updates

--- example_plot.py
+++ example_plot.py
@@ -1,20 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 plt.style.use('vyason')
 
-fig = plt.figure(constrained_layout=True)
+x,y1,y2 = np.loadtxt('table.d',unpack=True)
 
-table1 = np.genfromtxt('table1.d')
-plt.plot(table1[:,0],table1[:,1],c='black',ls='-',label='Table 1')
+plt.plot(x,y1,c='black',ls='-',label=r'$y_1$')
+plt.plot(x,y2,c='red',ls='--',label=r'$y_2$')
 
-table2 = np.genfromtxt('table2.d')
-plt.plot(table2[:,0],table2[:,1],c='red',ls='--',label='Table 2')
-
-plt.xlim(0,2)
-plt.ylim(-20,30)
-plt.xlabel(r'x')
-plt.ylabel(r'y')
-plt.grid()
-plt.legend()
+plt.xlim(0,2);      plt.ylim(-20,30)
+plt.xlabel(r'x');   plt.ylabel(r'y')
+plt.grid();         plt.legend()
 
 plt.savefig('example_plot.png')
